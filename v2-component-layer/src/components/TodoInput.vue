7bf6f369416f13242c5e495d555e3e38547a8d59
최다래 <darae@choedalaeui-MacBookPro.local>
2026-03-25 23:59:22 +0900

    [practice]:예제 타이핑

---
+++ v2-component-layer/src/components/TodoInput.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script setup>
+//composition API
+import { ref } from 'vue';
+
+const emit = defineEmits(['add-todo']);
+const todo = ref('');
+
+const addTodoHandler = () => {
+  if (todo.value.lenth >= 3) {
+    emit('add-todo', todo.value);
+    todo.value = '';
+  }
+};
+</script>
---
+++ v2-component-layer/src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import TodoListItem from './TodoListItem.vue';
+
+const props = defineProps({
+  todoList: { type: Array, required: true },
+});
+const emit = defineEmits(['delete-todo', 'toggle-completed']);
+</script>
+
+<template>
+  <div class="row">
+    <div class="col">
+      <ul class="list-group">
+        <TodoListItem
+          v-for="todoItem in todoList"
+          :key="todoItem.id"
+          :todoItem="todoItem"
+          @delete-todo="$emit('delete-todo', todoItem.id)"
+          @toggle-completed="$emit('toggle-compled', todoItem.id)"
+        />
+      </ul>
+    </div>
+  </div>
+</template>
