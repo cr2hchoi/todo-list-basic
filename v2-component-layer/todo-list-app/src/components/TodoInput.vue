f48c9a9c9897675950b778d0b05edfa3de9a34b2
cr2hChoi <20231046@sungshin.ac.kr>
2026-03-26 18:02:45 +0900

    [practice] 컴포넌트 연습

---
+++ v2-component-layer/todo-list-app/src/components/TodoInput.vue
@@ -0,0 +1,18 @@
+<script>
+  export default {
+    name: "TodoInput",
+  }
+</script>
+<template>
+  <div class="row mb-3">
+    <div class="col">
+      <div class="input-group">
+<!--        입력텍스트&버튼 그룹으로 묶어 제어-->
+        <input id="msg" type="text" class="form-control" name="msg"
+               placeholder="할 일 입력"/>
+<!--        사용자에게 입력값 받는 영역 지정-->
+        <span class="btn btn-primary input-group-addon">추가</span>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ v2-component-layer/todo-list-app/src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<script>
+import TodoListItem from './TodoListItem.vue'
+
+export default {
+  name:"TodoList", //컴포넌트 이름 지정. 안달아도 되지만 기본적으로 명시를 해주는게 좋다.
+  props:{todoList:{
+      type: Array,    // 배열로 받겠다고 꼭 명시
+      required: true  // 데이터를 꼭 받아야 출력시킬 수 있게 동작하겠다.
+      //required: false일 때 (기본값): 데이터를 안 줘도 에러는 안 나지만, 화면에 아무것도 안 나오거나 텅 빈 상태가 됨
+    }},
+  components:{TodoListItem}, //불러온 컴포넌트 등록
+}
+</script>
+<template>
+  <div class="row">
+    <div class="col">
+      <ul class="list-group">
+        <TodoListItem
+            v-for="todoItem in todoList"
+            :key="todoItem.id"
+            :todoItem="todoItem"
+        />
+      </ul>
+    </div>
+  </div>
+</template>
