9f5c2b8dfa1a6ceba2d3dabc89f30ba888a97095
cr2hchoi <20231046@sungshin.ac.kr>
2026-03-28 23:59:28 +0900

    Feat/step6 component practice (#4)
    * [#4]feat: index.html 내 입력창 및 리스트 하드코딩 추가
    
    * [#5]feat: Vue 인스턴스 생성 및 CRUD 메서드 정의
    
    * [#6] feat: Todo List 최종 기능 구현 및 목록 렌더링 성공
    
    * [practice] 컴포넌트 연습
    
    * feat/TodoInput.vue 컴포넌트 완성
    
    * feat/TodoInput.vue 컴포넌트 완성

---
+++ v2-component-layer/todo-list-app/src/components/TodoInput.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: 'TodoInput',
+  data() {
+    return { todo: '' };
+  },
+  emit: ['add-todo'],
+  methods: {
+    addTodoHandler() {
+      if (this.todo.length >= 3) {
+        //입력받은 글자가 3글자 이상일때?
+        this.$emit('add-todo', this.todo); //this.$emit('에밋명',전달할 데이터); this. 꼭 해줘야함
+        this.todo = ''; //입력 문자열 초기화
+      }
+    },
+  },
+};
+</script>
+<template>
+  <div class="row mb-3">
+    <div class="col">
+      <div class="input-group">
+        <!--        입력텍스트&버튼 그룹으로 묶어 제어-->
+        <input
+          id="msg"
+          type="text"
+          class="form-control"
+          name="msg"
+          placeholder="할 일 입력"
+          v-model.trim="todo"
+          @keydown.enter="addTodoHandler"
+        />
+        <!--        사용자에게 입력값 받는 영역 지정-->
+        <span class="btn btn-primary input-group-addon" @click="addTooHandler"
+          >추가</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
---
+++ v2-component-layer/todo-list-app/src/components/TodoListItem.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "TodoListItem",
+  props :{
+    todoItem:{type:Object, required:true},  //객체 타입으로 받겠다, 꼭 대문자로 시작해야함
+  }
+}
+</script>
+<template>
+  <li class="list-group-item" >
+    <span class="pointer">
+      <!--    데이터 바인딩할 영역-->
+      할일 명, 완료
+    </span>
+
+    <span class="float-end badge bg-secondary pointer"> 삭제</span>
+<!--    부트스트랩 요소 끝으로 밀어버리는 클래스-->
+  </li>
+</template>
