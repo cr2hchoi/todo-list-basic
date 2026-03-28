a6666364e44d6926a1c73cd9f272b6f0e2493048
choicr2h <20231046@sungshin.ac.kr>
2026-03-28 23:44:38 +0900

    feat/TodoInput.vue 컴포넌트 완성

--- v2-component-layer/todo-list-app/src/components/TodoInput.vue
+++ v2-component-layer/todo-list-app/src/components/TodoInput.vue
@@ -1,17 +1,36 @@
 <script>
-  export default {
-    name: "TodoInput",
+export default {
+  name: "TodoInput",
+  data(){return {todo:''}},
+  emit : { ['add-todo']},
+  methods : {
+    addTooHandler(){
+      if (this.todo.length >=3) { //입력받은 글자가 3글자 이상일때?
+        this.$emit('add-todo', this.todo); //this.$emit('에밋명',전달할 데이터); this. 꼭 해줘야함
+        this.todo=''; //입력 문자열 초기화
+      }
+    },
   }
+}
 </script>
 <template>
   <div class="row mb-3">
     <div class="col">
       <div class="input-group">
-<!--        입력텍스트&버튼 그룹으로 묶어 제어-->
-        <input id="msg" type="text" class="form-control" name="msg"
-               placeholder="할 일 입력"/>
-<!--        사용자에게 입력값 받는 영역 지정-->
-        <span class="btn btn-primary input-group-addon">추가</span>
+        <!--        입력텍스트&버튼 그룹으로 묶어 제어-->
+        <input
+          id="msg"
+          type="text"
+          class="form-control"
+          name="msg"
+          placeholder="할 일 입력"
+          v-model.trim="todo"
+          @keydown.enter="addTooHandler"
+        />
+        <!--        사용자에게 입력값 받는 영역 지정-->
+        <span class="btn btn-primary input-group-addon" @click="addTooHandler"
+          >추가</span
+        >
       </div>
     </div>
   </div>
